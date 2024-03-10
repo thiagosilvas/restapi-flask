113525b9045156771036390756ca73efdd13a73b
Thiago Silva <thiagodasilvasantos.tss@gmail.com>
2024-03-10 16:05:16 -0300

    feat: Initial flask restful project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # restapi-flask
+
+Comandos uteis:
+instalar o embiente virtual [VENV]: sudo apt install python3.10-venv
+criar ambiente virtual no diretorio atual: python3 -m venv .venv
+aponta as biblioteca para o ambiente virtual: source .venv/bin/activate
+
+verifica se alterou a pasta para o ambiente virtual: which python
+
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+    def get(self):
+        return {'message': 'user 1'}
+    def post(self, cpf):
+        return {'message': cpf}
+
+    
+class Users(Resource):
+    def get(self):
+        return {'message': 'all users'}    
+
+api.add_resource(User, '/user', '/user/<string>:cpf')
+api.add_resource(Users, '/users')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
